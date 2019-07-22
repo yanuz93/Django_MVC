6130a303963a40bf7e34332c1804a4675a08938d
yanuz93 <yanuzpurnomo@gmail.com>
2019-07-22 11:35:39 +0700

    coba-coba

---
+++ project_advance_views/env_project_advance_views/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta19/test/WebDev/Django/Django_MVC/project_advance_views/env_project_advance_views/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ project_model/env_project_model/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta19/test/WebDev/Django/Django_MVC/project_model/env_project_model/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
