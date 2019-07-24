8ca8dcf4639ab1de68a849f7e6908663c2a5f7b0
yanuz93 <yanuzpurnomo@gmail.com>
2019-07-24 09:45:01 +0700

    origin master

---
+++ project_views/env_project_views/bin/pa_reload_webapp.py
@@ -0,0 +1,29 @@
+#!/home/alta19/test/WebDev/Django/Django_MVC/project_views/env_project_views/bin/python3
+"""Reloads the given site
+
+Usage:
+  pa_reload_webapp.py <domain>
+
+Options:
+  <domain>              Domain name, eg www.mydomain.com
+"""
+
+from docopt import docopt
+import os
+import sys
+
+from pythonanywhere.api import Webapp
+from pythonanywhere.snakesay import snakesay
+
+
+def main(domain_name):
+    webapp = Webapp(domain_name)
+    webapp.reload()
+    print(snakesay(
+        "{} has been reloaded".format(domain_name)
+    ))
+
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    main(arguments['<domain>'])
